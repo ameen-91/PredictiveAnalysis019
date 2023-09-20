d417bcd0c1aac30a88a7dcedb34eba3e1136785b
norsu <mohammedameen9011@gmail.com>
2023-09-20 11:09:13 +0530

    dfg

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -4,6 +4,7 @@ import pandas as pd
 import time
 import joblib as jb
 from sklearn.pipeline import Pipeline
+import plotly.express as px
 
 st.set_page_config(
     page_title="Condition Monitoring",
@@ -14,9 +15,9 @@ st.set_page_config(
 
 data = pd.read_csv('demonstrate_data.csv')
 
-health = jb.load('predict.pkl')
-prep = jb.load('prep.pkl')
-anomaly = jb.load('anomaly.pkl')
+health = jb.load('PredictiveAnalysis019/predict.pkl')
+prep = jb.load('PredictiveAnalysis019/prep.pkl')
+anomaly = jb.load('PredictiveAnalysis019/anomaly.pkl')
 
 features = ['Pressure (psi)',	'Temperature (°C)',	'Oil Flow (L/min)',	'Vibration (mm/s)','Oil Level (%)'	,'Tool RPM (rpm)' ,'Current (A)']
 health_ft = ['Temperature (°C)','Tool RPM (rpm)']
@@ -99,15 +100,17 @@ def predict_anomaly(i):
 
 
 
-st.title("Condition Monitoring")
+st.title("Dashboard")
 update_interval = 1
 update_interval = st.slider(min_value=1,max_value=5,value=None,label='Speed')
 
 placeholder = st.empty()
 
-status = 'sdf'
+#status = 'sdf'
 
-for i in range(0,len(data)):
+#run = pd.DataFrame(columns=data.columns)
+
+for i in range(1,len(data)):
     checks(i)
     with placeholder.container():      
         element1,element2,element3 = st.columns(3)
@@ -118,14 +121,15 @@ for i in range(0,len(data)):
         d3,d4= st.columns(2)
         d5= st.empty()
         machine_health = st.empty()
-        element1.metric(value=data.loc[i,'Pressure (psi)'],label='Pressure (psi)')
-        element2.metric(value=data.loc[i,'Temperature (°C)'],label='Temperature (°C)')
-        element3.metric(value=data.loc[i,'Oil Flow (L/min)'],label='Oil Flow (L/min)')
-        e4.metric(value=data.loc[i,'Vibration (mm/s)'],label='Vibration (mm/s)')
+        ##fig_col1, fig_col2 = st.columns(2)
+        element1.metric(value=data.loc[i,'Pressure (psi)'],label='Pressure (psi)',delta=round((data.loc[i,'Pressure (psi)']-data.loc[i-1,'Pressure (psi)'])))
+        element2.metric(value=data.loc[i,'Temperature (°C)'],label='Temperature (°C)',delta=round((data.loc[i,'Temperature (°C)']-data.loc[i-1,'Temperature (°C)'])))
+        element3.metric(value=data.loc[i,'Oil Flow (L/min)'],label='Oil Flow (L/min)',delta=round((data.loc[i,'Oil Flow (L/min)']-data.loc[i-1,'Oil Flow (L/min)'])))
+        e4.metric(value=data.loc[i,'Vibration (mm/s)'],label='Vibration (mm/s)',delta=round((data.loc[i,'Vibration (mm/s)']-data.loc[i-1,'Vibration (mm/s)'])))
         e5.metric(value=data.loc[i,'Power Status'],label='Power Status')
-        e6.metric(value=data.loc[i,'Oil Level (%)'],label='Oil Level (%)')
-        e7.metric(value=data.loc[i,'Tool RPM (rpm)'],label='Tool RPM (rpm)')
-        e8.metric(value=data.loc[i,'Current (A)'],label='Current (A)')
+        e6.metric(value=data.loc[i,'Oil Level (%)'],label='Oil Level (%)',delta=round((data.loc[i,'Oil Level (%)']-data.loc[i-1,'Oil Level (%)'])))
+        e7.metric(value=data.loc[i,'Tool RPM (rpm)'],label='Tool RPM (rpm)',delta=round((data.loc[i,'Tool RPM (rpm)']-data.loc[i-1,'Tool RPM (rpm)'])))
+        e8.metric(value=data.loc[i,'Current (A)'],label='Current (A)',delta=round((data.loc[i,'Current (A)']-data.loc[i-1,'Current (A)'])))
         e9.metric(value=data.loc[i,'Machine Status'],label='Machine Status')
         e10.metric(value=predict_anomaly(i),label='Anomaly')
         d1.write(f"Pressure Levels: {pressure_cond}")
@@ -134,6 +138,17 @@ for i in range(0,len(data)):
         d4.write(f"RPM Levels: {rpm_cond}")
         d5.write(f"Vibration Levels: {vib_cond}")
         machine_health.metric(value=predict_health(i),label='Temperature/RPM Failure')
+        new_row = pd.Series(data.loc[i,data.columns])
+        #un = run.append(new_row, ignore_index=True)
+
+        
+        '''with fig_col1:
+            st.markdown("### First Chart")
+            fig = px.bar(data_frame=data, y = data'Pressure (psi)')
+            st.write(fig)
+        with fig_col2:
+            st.markdown("### Second Chart")
+            fig2 = px.histogram(data_frame = data, y = 'Temperature (°C)')'''
 
         
         time.sleep(1/update_interval)
